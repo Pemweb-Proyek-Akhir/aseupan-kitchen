388998a44f67f8ae1e37a9d2c89810665737eacf
khansalaksono <khansalaksono@gmail.com>
2023-06-25 14:24:13 +0700

    dashboardAdmin update

--- resources/views/dashboardAdmin.blade.php
+++ resources/views/dashboardAdmin.blade.php
@@ -24,7 +24,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Selamat datang di Dashboard Admin!") }}
+                    {{ __("Welcome to Admin\'s Dashboard!") }}
                 </div>
                 <div class="menu-container">
                     <a href="{{ route('admin.index') }}" class="menu-button">Manage Products</a>
